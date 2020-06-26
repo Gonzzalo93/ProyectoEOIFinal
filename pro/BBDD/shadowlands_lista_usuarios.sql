674039bbc9db503a5648150a7955c5164c15971e
Santi <santymiu@gmail.com>
2020-06-26 16:25:09 +0200

    MySQL Test

--- pro/BBDD/shadowlands_lista_usuarios.sql
+++ pro/BBDD/shadowlands_lista_usuarios.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `shadowlands` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `shadowlands`;
 -- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
 -- Host: localhost    Database: shadowlands
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-26 16:00:33
+-- Dump completed on 2020-06-26 16:24:08
