5fd3f370bb20c9a0d996c99742e207afb57556c5
Miguel <miguel_mgarrido@hotmail.com>
2020-07-06 15:31:51 +0200

    Servlets arreglados

--- pro/Source Package/web/ServletCrearMago.java
+++ pro/Source Package/web/ServletCrearMago.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/ServletCrearMago")
 public class ServletCrearMago extends HttpServlet{
       @Override
         protected void doPost(HttpServletRequest request, HttpServletResponse response)throws IOException{
